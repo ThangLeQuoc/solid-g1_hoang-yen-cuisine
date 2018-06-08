e3415b97a2bf14a48a3c6835977068850a5c650d
Phúc Hồng Võ <phuc.vo@adnovum.vn>
2018-06-08 09:02:43 +0700

    Add sample dish

---
+++ src/test/java/hoang_yen_cuisine/basic/LunchOrderProcessorTest.java
@@ -0,0 +1,11 @@
+
+package hoang_yen_cuisine.basic;
+
+public class LunchOrderProcessorTest {
+
+	private LunchOrderProcessor testee = new LunchOrderProcessor();
+	
+	public void foo() {
+		testee.makeLunchOrder(null, null);
+	}
+}
